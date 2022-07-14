210cca7dbedfa59644ee86d957a93f9607f83152
Thanos <74588018+thanosoncode@users.noreply.github.com>
2022-07-14 10:51:29 +0300

    initial commit

---
+++ src/context.tsx
@@ -0,0 +1,55 @@
+import React, { useReducer } from "react";
+
+type AppState = {
+  globalVar: string;
+};
+
+const defaultState: AppState = {
+  globalVar: "react ftw",
+};
+
+type Action = { type: "CHANGE_GLOBAL_VAR"; updatedVar: string };
+
+const appReducer = (state: AppState, action: Action) => {
+  switch (action.type) {
+    case "CHANGE_GLOBAL_VAR":
+      return { ...state, globalVar: action.updatedVar };
+
+    default:
+      return state;
+  }
+};
+
+type Dispatch = (action: Action) => void;
+type AppProviderProps = { children: React.ReactNode };
+
+const AppStateContext = React.createContext<AppState | undefined>(undefined);
+const AppDispatchContext = React.createContext<Dispatch | undefined>(undefined);
+
+const AppProvider = ({ children }: AppProviderProps) => {
+  const [state, dispatch] = useReducer(appReducer, defaultState);
+
+  return (
+    <AppStateContext.Provider value={state}>
+      <AppDispatchContext.Provider value={dispatch}>
+        {children}
+      </AppDispatchContext.Provider>
+    </AppStateContext.Provider>
+  );
+};
+
+const useAppState = () => {
+  const context = React.useContext(AppStateContext);
+  if (context === undefined) {
+    throw new Error("useAppState must be used inside AppProvider");
+  }
+};
+
+const useAppDispatch = () => {
+  const context = React.useContext(AppDispatchContext);
+  if (context === undefined) {
+    throw new Error("useAppDispatch must be used inside AppProvider");
+  }
+};
+
+export { AppProvider, useAppState, useAppDispatch };
